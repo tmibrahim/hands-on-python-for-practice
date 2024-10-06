49a3a08ea85a8a563e9484b3d9e34f75beb83a9b
İbrahim TEMUR <ibrahim-temur@outlook.com>
2024-10-06 10:41:41 +0000

    01-03 practices added.

---
+++ Ex_Files/02_02_begin/main.py
@@ -0,0 +1,12 @@
+greet = "Hello World"
+extened_grt = "Hello World, " + "this is a long string"
+
+name = "John"
+
+intrupution = f"Hello {name}"
+
+greet_format = "Hello {}"
+
+formatted = greet_format.format(name)
+
+print(intrupution, formatted)
